1b13c418c88719a70489679a4fea4fad3d9008bb
dinoosauroYS <felipehfigueiredo@gmail.com>
2023-10-23 23:04:10 -0300

    fiz a calculadora de multiplicação

--- src/App.java
+++ src/App.java
@@ -2,6 +2,14 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner sc = Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Qual o primeiro numero que voce quer multiplicar?");
+        int x = sc.nextInt();
+        System.out.println("Qual o segundo numero que voce quer multiplicar?");
+        int y = sc.nextInt();
+        System.out.println(x * y);
+        
+        sc.close();
+
     }
 }
